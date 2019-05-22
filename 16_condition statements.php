c1bd66c958f810b86b9b4413e467bd82e4889763
Taen Ahammed <taenahammed7@gmail.com>
2019-05-22 16:03:41 +0600

    conditional statement

---
+++ 16_condition statements.php
@@ -0,0 +1,9 @@
+<?php
+$role = "Visitor";
+
+if ($role === "Admin")
+    echo ("You are a Admin \n");
+elseif ($role === "Moderator")
+    echo ("You are a Moderator \n");
+else
+    echo ("You are a Guest \n");
