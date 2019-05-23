a3f0b6b599bd96115ddedba1cfdbcc1ea245b28b
Taen Ahammed <taenahammed7@gmail.com>
2019-05-23 16:21:14 +0600

    $_SERVER

--- 25_superglobals.php
+++ 25_superglobals.php
@@ -8,4 +8,19 @@ function superGlobal()
     $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
 }
 superGlobal();
-echo ($z);
+echo ("{$z} <br/>");
+
+// Returns the filename of the currently executing script
+echo $_SERVER['PHP_SELF'] . '<br/>';
+
+// Returns the name of the host server
+echo $_SERVER['SERVER_NAME'] . '<br/>';
+
+// Returns the path of the current script
+echo $_SERVER['SCRIPT_NAME'] . '<br/>';
+
+// Returns the client information
+echo $_SERVER['HTTP_USER_AGENT'] . '<br/>';
+
+// Returns the IP address of the host server
+echo $_SERVER['SERVER_ADDR'];
