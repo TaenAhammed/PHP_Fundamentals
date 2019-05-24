c94e1c6b96c3b59c9b8f6b8f665db313a3cfaba2
Taen Ahammed <taenahammed7@gmail.com>
2019-05-24 11:08:33 +0600

    debugger added and while loop.

--- 18_while & do while loops.php
+++ 18_while & do while loops.php
@@ -1,13 +1,23 @@
 <?php
 
-$x = 1;
-while ($x < 10) {
-    echo ($x . "\n");
-    $x++;
-}
+// $x = 1;
+// while ($x < 10) {
+//     echo ($x . "\n");
+//     $x++;
+// }
 
-$y = 10;
-do {
-    echo ($y . "\n");
-    $y++;
-} while ($y < 5);
+// $y = 10;
+// do {
+//     echo ($y . "\n");
+//     $y++;
+// } while ($y < 5);
+
+
+// Where we don't need initialization & increment there we can use while loop otherwise we can for loop.
+
+$a = 10;
+
+while ($a > 0) {
+    $a = (int)($a / 2);
+    echo "Divided <br/>";
+}
